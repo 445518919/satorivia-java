73def38ba436a1bf7d85aa3e49dfc3bb381bb2ed
qqwq <445518919@qq.com>
2026-06-25 12:30:15 +0800

    refactor(domain): 删除 ProductDetail 数据类
    - 移除不再使用的 ProductDetail 类及其相关字段
    - 精简代码库，减少冗余代码
    - 改善项目结构与可维护性

--- satoriviacafe-cafe/src/main/kotlin/com/satoriviacafe/cafe/domain/ProductDetail.kt
+++
@@ -1,9 +0,0 @@
-package com.satoriviacafe.cafe.domain
-
-import com.alipay.mychain.taas.api.response.h5.TpCodeInfoResp
-
-
-data class ProductDetail(
-    private val displayData: CafeProduction,
-    private var blockchainData: TpCodeInfoResp,
-)
