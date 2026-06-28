e6cded7cf4697812901564dee8426a86cc679528
qqwq <445518919@qq.com>
2026-06-28 20:01:50 +0800

    feat(domain): 添加NfcImportDTO的Excel注解支持
    - 在NfcImportDTO类中引入Excel注解
    - 为nfcCode字段添加"NFC编码"的Excel注解
    - 为skuCode字段添加"SKU编码"的Excel注解
    - 为productName字段添加"产品名称"的Excel注解
    - 为batchName字段添加"批次名称"的Excel注解
    - 补充字段注释，增强代码可读性和文档生成支持

--- satoriviacafe-cafe/src/main/java/com/satoriviacafe/cafe_web/domain/NfcImportDTO.java
+++ satoriviacafe-cafe/src/main/java/com/satoriviacafe/cafe_web/domain/NfcImportDTO.java
@@ -1,6 +1,8 @@
 package com.satoriviacafe.cafe_web.domain;
 
+import com.satoriviacafe.common.annotation.Excel;
 import lombok.Data;
+
 
 /**
  * NFC导入DTO
@@ -10,8 +12,24 @@ import lombok.Data;
  */
 @Data
 public class NfcImportDTO {
+    /**
+     * NFC编码
+     */
+    @Excel(name = "NFC编码")
     private String nfcCode;
+    /**
+     * SKU编码
+     */
+    @Excel(name = "SKU编码")
     private String skuCode;
+    /**
+     * 产品名称
+     */
+    @Excel(name = "产品名称")
     private String productName;
+    /**
+     * 批次名称
+     */
+    @Excel(name = "批次名称")
     private String batchName;
 }
